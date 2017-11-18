57a6449e975b1c6191a22d82e09fca4f01f91200
jerry cui <jerrycui@10.0.0.6>
2017-11-18 21:23:17 +0800

    add tool interface

---
+++ tools/toolInterface.hpp
@@ -0,0 +1,17 @@
+#ifdef __cplusplus 
+extern "C"
+{
+#endif
+
+
+#ifdef --cplusplus 
+}
+#endif
+
+
+class toolInterface {
+	public:
+		virtual void createWorld() = 0;
+	private:
+		std::string version__;
+};
