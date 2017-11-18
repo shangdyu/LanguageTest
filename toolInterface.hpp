73dbf95efe1c0b05af08b06a8764fec0d67cf327
jerry cui <jerrycui@10.0.0.6>
2017-11-18 21:36:28 +0800

    Squashed 'tools/' changes from a8f0108..d859724
    d859724 add a no use file
    7099d3e fix error
    ef01712 add tool interface
    
    git-subtree-dir: tools
    git-subtree-split: d859724426e583d607402899a29bf3c5c973c128

---
+++ toolInterface.hpp
@@ -0,0 +1,17 @@
+#ifdef __cplusplus 
+extern "C"
+{
+#endif
+
+
+#ifdef __cplusplus 
+}
+#endif
+
+
+class toolInterface {
+	public:
+		virtual void createWorld() = 0;
+	private:
+		std::string version__;
+};
